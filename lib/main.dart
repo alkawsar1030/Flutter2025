29e5dfb05821c998e862bf18385d5011f4291ed1
Kawsar <alkawsar1030@gmail.com>
2025-10-11 00:49:23 +0600

    Elevated, Outlined, Text, Icon Button

--- lib/main.dart
+++ lib/main.dart
@@ -16,20 +16,49 @@ class App extends StatelessWidget {
 class Homepage extends StatelessWidget {
   const Homepage({super.key});
 
-  void showSnack(BuildContext context, String msg) {
-    ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text(msg)));
-  }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Center(child: Text('Title Bar of My first App'))),
-      body: ElevatedButton(
-        onPressed: () {
-          showSnack(context, 'This is Elevated Button');
-        },
-        child: Text('Click ME'),
+      body: Center(
+        child: Column(
+          children: [
+            ElevatedButton(
+              onPressed: () {
+                showSnack(context, 'This is Elevated Button');
+              },
+              child: Text('Click ME'),
+            ),
+
+            SizedBox(height: 5),
+            OutlinedButton(
+              onPressed: () {
+                showSnack(context, 'This is Outlined Button');
+              },
+              child: Text('Click ME'),
+            ),
+            SizedBox(height: 5),
+            TextButton(
+              onPressed: () {
+                showSnack(context, 'This is Text Button');
+              },
+              child: Text('Click ME'),
+            ),
+
+            SizedBox(height: 5),
+            IconButton(
+              onPressed: () {
+                showSnack(context, 'This is icon Button');
+              },
+              icon: Icon(Icons.icecream_outlined),
+            ),
+          ],
+        ),
       ),
     );
+  }
+
+  void showSnack(BuildContext context, String msg) {
+    ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text(msg)));
   }
 }
